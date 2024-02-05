8dd28aa33f6bfa6310db55433ed2997df3befb00
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2024-02-05 22:51:19 +0200

    Update flutter

--- flake.nix
+++ flake.nix
@@ -1,61 +1,49 @@
 {
-  description = "My Android project";
+  description = "Flutter project for tracking finances";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/23.11";
-    devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
-    android.url = "github:tadfisher/android-nixpkgs";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    android.url = "github:tadfisher/android-nixpkgs/stable";
   };
 
-  outputs = { self, nixpkgs, devshell, flake-utils, android }:
-    {
-      overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk android-studio;
+  outputs = { self, nixpkgs, unstable, android }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
       };
-    }
-    //
-    flake-utils.lib.eachSystem [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system:
-      let
-        inherit (nixpkgs) lib;
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            devshell.overlays.default
-            self.overlay
-          ];
-        };
-      in
-      {
-        packages = {
-          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
-            # Useful packages for building and testing
-            cmdline-tools-latest
-            emulator
-            platform-tools
 
-            build-tools-33-0-1
-            sources-android-33
-            platforms-android-33
-          ]
-          ++ lib.optionals (system == "aarch64-darwin") [
-            # system-images-android-34-google-apis-arm64-v8a
-            # system-images-android-34-google-apis-playstore-arm64-v8a
-          ]
-          ++ lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
-            # system-images-android-34-google-apis-x86-64
-            # system-images-android-34-google-apis-playstore-x86-64
-          ]);
-        } // lib.optionalAttrs (system == "x86_64-linux") {
-          # Android Studio in nixpkgs is currently packaged for x86_64-linux only.
-          android-studio = pkgs.androidStudioPackages.stable;
-          # android-studio = pkgs.androidStudioPackages.beta;
-          # android-studio = pkgs.androidStudioPackages.preview;
-          # android-studio = pkgs.androidStudioPackage.canary;
-        };
+      unstable-pkgs = import unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
-        devShell = import ./devshell.nix { inherit pkgs; };
-      }
-    );
+      android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+        cmdline-tools-latest
+        emulator
+        platform-tools
+
+        build-tools-33-0-1
+        sources-android-33
+        platforms-android-33
+      ]);
+    in
+    {
+      devShells.${system}.default = with pkgs; mkShell {
+        packages = [
+          unstable-pkgs.flutter
+          android-sdk
+          androidStudioPackages.stable
+        ];
+
+        shellHook = ''
+          ${unstable-pkgs.flutter}/bin/flutter --disable-analytics > /dev/null
+          source <(${unstable-pkgs.flutter}/bin/flutter bash-completion)
+          export PS1='\[\e[1;93m\][Finances]\[\e[m\] \$ '
+        '';
+      };
+    };
 }
