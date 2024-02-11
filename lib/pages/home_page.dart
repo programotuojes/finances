fa532f27974eb8caae04572f98d22ced83a8fa8c
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2024-02-11 22:40:53 +0200

    A bit prettier expense list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:finances/account/pages/list.dart';
 import 'package:finances/category/pages/list.dart';
 import 'package:finances/category/service.dart';
 import 'package:finances/expense/pages/add.dart';
+import 'package:finances/expense/pages/edit.dart';
 import 'package:finances/expense/service.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,27 +23,30 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Finances'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: ListenableBuilder(
-          listenable: ExpenseService.instance,
-          builder: (context, _) {
-            return ListView(
-              children: [
-                for (var i in ExpenseService.instance.expenses)
-                  ListTile(
-                    title: Text(i.category.name),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (i.description != null) Text(i.description!),
-                        Text(i.amount.toString()),
-                        Text(i.account.name),
-                        Text(i.dateTime.toString()),
-                      ],
-                    ),
-                  ),
-              ],
-            );
-          }),
+      body: [
+        home(),
+        history(),
+      ][index],
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        selectedIndex: index,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.receipt_long),
+            icon: Icon(Icons.receipt_long_outlined),
+            label: 'History',
+          )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -90,6 +96,64 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget home() {
+    return const Placeholder();
+  }
+
+  Widget history() {
+    return ListenableBuilder(
+      listenable: ExpenseService.instance,
+      builder: (context, _) {
+        if (ExpenseService.instance.expenses.isEmpty) {
+          return const Center(child: Text('There are no expenses'));
+        }
+        return ListView(
+          children: [
+            for (var i in ExpenseService.instance.expenses)
+              ListTile(
+                title: Text(i.category.name),
+                leading: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  child: Icon(
+                    Icons.food_bank,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+                trailing: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(i.account.name),
+                    Text(i.amount.toString()),
+                  ],
+                ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(i.dateTime.toString().substring(0, 16)),
+                    if (i.description != null) Text(i.description!),
+                  ],
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditExpensePage(i),
+                    ),
+                  );
+                },
+              ),
+          ],
+        );
+      },
     );
   }
 }
