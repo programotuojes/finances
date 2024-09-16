a287babc54a209935b98bd4e1af6f2f65f6e87cc
Gustas Klevinskas <programotuojes@users.noreply.github.com>
2024-09-16 18:42:21 +0300

    chore: remove Gradle patch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     android = {
       url = "github:tadfisher/android-nixpkgs/stable";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,11 +17,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
       pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
 
-      flutter = pkgs-unstable.flutterPackages.v3_24.override (prev: {
-        flutter = prev.flutter.override (prev: {
-          patches = prev.patches ++ [ ./gradle-flutter-tools-wrapper.patch ];
-        });
-      });
+      flutter = pkgs-unstable.flutterPackages.v3_24;
 
       android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
         cmdline-tools-latest
@@ -34,14 +30,16 @@
         platforms-android-33
         platforms-android-34
       ]);
+
+      ld-library-path = pkgs.lib.makeLibraryPath [ pkgs.sqlite ];
     in
     {
-      packages.${system}.default = with pkgs; flutter.buildFlutterApplication {
+      packages.${system}.default = flutter.buildFlutterApplication {
         pname = "finances";
         version = "0.1.0";
         src = ./.;
         autoPubspecLock = ./pubspec.lock;
-        extraWrapProgramArgs = "--suffix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ sqlite ]}";
+        extraWrapProgramArgs = "--suffix LD_LIBRARY_PATH : ${ld-library-path}";
       };
 
       devShells.${system}.default = with pkgs; mkShell {
@@ -53,6 +51,7 @@
 
         FLUTTER_SDK = flutter;
         JAVA_HOME = jdk17.home;
+        LD_LIBRARY_PATH=ld-library-path;
 
         shellHook = ''
           ${flutter}/bin/flutter config --disable-analytics --jdk-dir ${jdk17} > /dev/null
@@ -62,7 +61,6 @@
 
           # Avoid crashing when opening file picker
           export XDG_DATA_DIRS=${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
-          export LD_LIBRARY_PATH=${lib.makeLibraryPath [ sqlite ]}:$LD_LIBRARY_PATH
         '';
       };
     };
